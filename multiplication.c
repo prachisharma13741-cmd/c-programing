44f4daeb489f6b22e444ec851f1606ca0a7323e2
prachisharma13741-cmd <prachi.sharma13741@gmail.com>
2025-08-13 11:47:38 +0530

    Add files via upload

---
+++ multiplication.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+int num1,num2,sub ;
+printf("Enter two number \n ");
+scanf("%d %d",&num1, &num2);
+sub=num1*num2 ;
+printf("multiple if %d and %d= %d\n", num1,num2,sub);
+
+}
