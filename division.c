7c48983fbb101e245381c9a2a873e33690d39edb
prachisharma13741-cmd <prachi.sharma13741@gmail.com>
2025-08-13 11:54:40 +0530

    Add files via upload

---
+++ division.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+int num1,num2,div ;
+printf("Enter two number \n ");
+scanf("%d %d",&num1, &num2);
+div=num1/num2 ;
+printf("divide if %d and %d= %d\n", num1,num2,div);
+
+}
