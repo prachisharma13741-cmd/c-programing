f17e4e8ad66ce7bc3386f252960c47ef1214e15a
prachisharma13741-cmd <prachi.sharma13741@gmail.com>
2025-08-13 11:53:47 +0530

    Update multiplication.c

--- multiplication.c
+++ multiplication.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main() {
-int num1,num2,sub ;
+int num1,num2,prod ;
 printf("Enter two number \n ");
 scanf("%d %d",&num1, &num2);
 prod=num1*num2 ;
 printf("multiple if %d and %d= %d\n", num1,num2,prod);
 
 }
+
 
