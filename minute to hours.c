b003cebda46c203f4623d00b1115d965a11ca84b
prachisharma13741-cmd <prachi.sharma13741@gmail.com>
2025-09-03 12:15:49 +0530

    Add files via upload

---
+++ minute to hours.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    int m;
+    printf("Enter minute: ");
+    scanf("%d", &m);
+    printf("%d minute = %d hours %d minute", m, m/60, ,m%60);
+    return 0;
+
+
+}
